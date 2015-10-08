3b1ec53c461308cd4b26203c94d9ce940b7af37d
Kevin Kellar <flipturnapps@gmail.com>
2015-10-07 20:28:36 -0700

    Created a vector and positioner

---
+++ src/Positioner.java
@@ -0,0 +1,34 @@
+
+public class Positioner
+{
+	private Vector currentPosition;
+	private Vector currentVelocity;
+	public Positioner ()
+	{
+		currentPosition = new Vector();
+		currentVelocity = new Vector();
+	}
+	public Positioner (int x, int y, int z)
+	{
+		this();
+		Vector pos = new Vector();
+		pos.setX(x);
+		pos.setY(y);
+		pos.setZ(z);
+		currentPosition = pos;
+	}
+	public Positioner (Vector pos)
+	{
+		this();
+		currentPosition = pos;
+	}
+	public Vector getPosition()
+	{
+		return currentPosition;
+	}
+	public void actOnSample(Vector acceleration, double timeElapsed)
+	{
+		//Change velocity based on vx = v0 + at
+		cur
+	}
+}
